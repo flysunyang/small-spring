5e8e57b53a5c55c57874fde2debd444d33b83862
zhaoyaxu <zhaoyaxu@cisco.com>
2025-05-28 16:04:25 +0800

    添加 beanDefinition

---
+++ src/main/java/org/springframework/beans/factory/BeanFactory.java
@@ -0,0 +1,9 @@
+package org.springframework.beans.factory;
+
+import org.springframework.beans.BeansException;
+
+public interface BeanFactory {
+
+    Object getBean(String beanName) throws BeansException;
+
+}
