caaa4204faef3c673ad60799a014aaa0efcfdf17
zhaoyaxu <zhaoyaxu@cisco.com>
2025-06-18 13:55:00 +0800

    cglib proxy

---
+++ src/main/java/org/springframework/aop/framework/CglibAopProxy.java
@@ -0,0 +1,60 @@
+package org.springframework.aop.framework;
+
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.MethodInterceptor;
+import net.sf.cglib.proxy.MethodProxy;
+import org.springframework.aop.AdvisedSupport;
+
+import java.lang.reflect.Method;
+
+public class CglibAopProxy implements AopProxy {
+
+    private final AdvisedSupport advised;
+
+    public CglibAopProxy(AdvisedSupport advised) {
+        this.advised = advised;
+    }
+
+    @Override
+    public Object getProxy() {
+        Enhancer enhancer = new Enhancer();
+        enhancer.setSuperclass(advised.getTargetSource().getTarget().getClass());
+        enhancer.setInterfaces(advised.getTargetSource().getTargetClass());
+        enhancer.setCallback(new DynamicAdvisedInterceptor(advised));
+        return enhancer.create();
+    }
+
+    private static class DynamicAdvisedInterceptor implements MethodInterceptor {
+
+        private final AdvisedSupport advised;
+
+        public DynamicAdvisedInterceptor(AdvisedSupport advised) {
+            this.advised = advised;
+        }
+
+        @Override
+        public Object intercept(Object obj, Method method, Object[] args, MethodProxy proxy) throws Throwable {
+            CglibMethodInvocation methodInvocation = new CglibMethodInvocation(advised.getTargetSource().getTarget(), method, args, proxy);
+            if (advised.getMethodMatcher().matches(method, Object.class)) {
+                return advised.getMethodInterceptor().invoke(methodInvocation);
+            }
+            return methodInvocation.proceed();
+        }
+
+    }
+
+    private static class CglibMethodInvocation extends ReflectiveMethodInvocation {
+
+        private final MethodProxy methodProxy;
+
+        public CglibMethodInvocation(Object target, Method method, Object[] arguments, MethodProxy methodProxy) {
+            super(target, method, arguments);
+            this.methodProxy = methodProxy;
+        }
+
+        @Override
+        public Object proceed() throws Throwable {
+            return methodProxy.invoke(target, arguments);
+        }
+    }
+}
--- src/test/java/org/springframework/test/aop/DynamicProxyTest.java
+++ src/test/java/org/springframework/test/aop/DynamicProxyTest.java
@@ -1,10 +1,12 @@
 package org.springframework.test.aop;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.AdvisedSupport;
 import org.springframework.aop.MethodMatcher;
 import org.springframework.aop.TargetSource;
 import org.springframework.aop.aspectj.AspectJExpressionPointcut;
+import org.springframework.aop.framework.CglibAopProxy;
 import org.springframework.aop.framework.JdkDynamicAopProxy;
 import org.springframework.test.common.WorldServiceInterceptor;
 import org.springframework.test.service.WorldService;
@@ -12,19 +14,30 @@ import org.springframework.test.service.WorldServiceImpl;
 
 public class DynamicProxyTest {
     
-    @Test
-    void testJdkDynamicProxy() {
+    private AdvisedSupport advisedSupport;
+    
+    @BeforeEach
+    void setup() {
         WorldService worldService = new WorldServiceImpl();
-        
-        AdvisedSupport advisedSupport = new AdvisedSupport();
+
+        advisedSupport = new AdvisedSupport();
         TargetSource targetSource = new TargetSource(worldService);
         WorldServiceInterceptor methodInterceptor = new WorldServiceInterceptor();
         MethodMatcher methodMatcher = new AspectJExpressionPointcut("execution(* org.springframework.test.service.WorldService.explode(..))");
         advisedSupport.setTargetSource(targetSource);
         advisedSupport.setMethodInterceptor(methodInterceptor);
         advisedSupport.setMethodMatcher(methodMatcher);
-        
+    }
+    
+    @Test
+    void testJdkDynamicProxy() {
         WorldService proxy = (WorldService) new JdkDynamicAopProxy(advisedSupport).getProxy();
+        proxy.explode();
+    }
+    
+    @Test
+    void testCglibDynamicProxy() {
+        WorldService proxy = (WorldService) new CglibAopProxy(advisedSupport).getProxy();
         proxy.explode();
     }
 }
